0390e0975e08c054e8cd2cbf1c97a32e5e82096f
Ernesto Loynaz <eloynaz@Ernestos-MacBook-Pro.local>
2014-10-05 10:55:26 -0400

    second commit

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Git test
+//
+//  Created by Ernesto Loynaz on 10/5/14.
+//  Copyright (c) 2014 Ernesto Loynaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : Character
+
+@end
---
+++ test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Git test
+//
+//  Created by Ernesto Loynaz on 10/5/14.
+//  Copyright (c) 2014 Ernesto Loynaz. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
